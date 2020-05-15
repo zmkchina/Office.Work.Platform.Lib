77ef5c11d626cdc4565bf9ca7db48e6b21e4c9aa
mrzmkOffice <mrzmk@Office>
2020-05-15 18:53:25 +0800

    FromOffice 20200515

---
+++ AppUpdateInfo.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Office.Work.Platform.Lib
+{
+    /// <summary>
+    /// 升级文件描述类
+    /// </summary>
+    public class AppUpdateInfo
+    {
+        /// <summary>
+        /// 欲升级的时间信息，如客户端保存的升级时间比这个时间早，则启动升级程序
+        /// </summary>
+        public DateTime UpdateDate { get; set; }
+        /// <summary>
+        /// 欲升级的文件名称列表
+        /// </summary>
+        public List<string> UpdateFiles { get; set; }
+        /// <summary>
+        /// 服务器读取升级信息结果
+        /// </summary>
+        public bool ReadResult { get; set; }
+        /// <summary>
+        /// 构造函数，初始化UpdateDate为最小值
+        /// </summary>
+        public AppUpdateInfo()
+        {
+            UpdateDate = DateTime.MinValue;
+            UpdateFiles = new List<string>();
+            ReadResult = false;
+        }
+    }
+}
